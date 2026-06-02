abaa7246173b7933c2f6683f855b7089fb84b82c
Raiyan109 <kraiyan109@gmail.com>
2026-06-02 10:48:17 +0600

    unneccessary modules delted

--- src/modules/Search/search.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Types } from "mongoose";
-import { IUserInterface } from "../User/user.interface";
-
-export interface ISearch{
-    user_id: Types.ObjectId | IUserInterface;
-    searchTerm: string;
-    searchedAt: Date;
-}
--- src/modules/Wishlist/wishlist.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Types } from "mongoose";
-import { IProductInterface } from "../Product/product.interface";
-
-export interface IWishlist {
-    product_id: (IProductInterface | Types.ObjectId)[];
-    user_id: Types.ObjectId;
-}
